338e9e5531de7c0ca5badcb9ce77f6be15502c96
ibrahimcbc <icebecioglu21@ku.edu.tr>
2025-09-03 21:24:33 +0300

    JPAUtil implementation

--- src/main/java/com/example/config/JPAUtil.java
+++ src/main/java/com/example/config/JPAUtil.java
@@ -1,4 +1,19 @@
 package com.example.config;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class JPAUtil {
+    private static final EntityManagerFactory emf =
+            Persistence.createEntityManagerFactory("corpPU");
+
+    public static EntityManagerFactory getEntityManagerFactory() {
+        return emf;
+    }
+
+    public static void shutdown() {
+        if (emf != null && emf.isOpen()) {
+            emf.close();
+        }
+    }
 }

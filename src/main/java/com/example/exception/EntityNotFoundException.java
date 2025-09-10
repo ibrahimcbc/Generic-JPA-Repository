d71813cd414d54c8694d899ff60891ff4e871cde
ibrahimcbc <icebecioglu21@ku.edu.tr>
2025-09-10 23:45:51 +0300

    commit

--- src/main/java/com/example/exception/EntityNotFoundException.java
+++ src/main/java/com/example/exception/EntityNotFoundException.java
@@ -1,5 +1,8 @@
 package com.example.exception;
 
+/**
+ * Exception class for when the entity not found
+ */
 public class EntityNotFoundException extends RuntimeException {
     public EntityNotFoundException(String message) {
         super(message);
--- src/main/java/com/example/exception/InvalidEntityException.java
+++ src/main/java/com/example/exception/InvalidEntityException.java
@@ -1,5 +1,8 @@
 package com.example.exception;
 
+/**
+ * Exception class for when the entity has an invalid class
+ */
 public class InvalidEntityException extends RuntimeException {
     public InvalidEntityException(String message) {
         super(message);
